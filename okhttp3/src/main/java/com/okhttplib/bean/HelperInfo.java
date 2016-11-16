0f012d6d8995a46cd550ca0f3b0123ca1c90b9a9
zhsf <442553199@qq.com>
2016-11-16 18:33:23 +0800

    增加辅助类

---
+++ okhttp3/src/main/java/com/okhttplib/bean/HelperInfo.java
@@ -0,0 +1,90 @@
+package com.okhttplib.bean;
+
+import com.okhttplib.interceptor.ExceptionInterceptor;
+import com.okhttplib.interceptor.ResultInterceptor;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * 辅助类信息体
+ * @author: zhousf
+ */
+public class HelperInfo {
+
+    private String LogTAG;//打印日志标识
+    private long timeStamp;//时间戳
+    private boolean showHttpLog;//是否显示Http请求日志
+    private OkHttpClient httpClient;
+    private Class<?> tag;//请求标识
+    private List<ResultInterceptor> resultInterceptors;//请求结果拦截器
+    private List<ExceptionInterceptor> exceptionInterceptors;//请求链路异常拦截器
+    private String downloadFileDir;//下载文件保存目录
+
+
+    public String getLogTAG() {
+        return LogTAG;
+    }
+
+    public void setLogTAG(String logTAG) {
+        LogTAG = logTAG;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public boolean isShowHttpLog() {
+        return showHttpLog;
+    }
+
+    public void setShowHttpLog(boolean showHttpLog) {
+        this.showHttpLog = showHttpLog;
+    }
+
+    public OkHttpClient getHttpClient() {
+        return httpClient;
+    }
+
+    public void setHttpClient(OkHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public Class<?> getTag() {
+        return tag;
+    }
+
+    public void setTag(Class<?> tag) {
+        this.tag = tag;
+    }
+
+    public List<ResultInterceptor> getResultInterceptors() {
+        return resultInterceptors;
+    }
+
+    public void setResultInterceptors(List<ResultInterceptor> resultInterceptors) {
+        this.resultInterceptors = resultInterceptors;
+    }
+
+    public List<ExceptionInterceptor> getExceptionInterceptors() {
+        return exceptionInterceptors;
+    }
+
+    public void setExceptionInterceptors(List<ExceptionInterceptor> exceptionInterceptors) {
+        this.exceptionInterceptors = exceptionInterceptors;
+    }
+
+    public String getDownloadFileDir() {
+        return downloadFileDir;
+    }
+
+    public void setDownloadFileDir(String downloadFileDir) {
+        this.downloadFileDir = downloadFileDir;
+    }
+
+}
