96fc9f57cd61c8633433a8ea4ff8db549e3955ee
zhousf <424427633@qq.com>
2017-07-20 16:50:28 +0800

    采用provider方式初始化第三方组件

--- app/src/main/java/base/BaseApplication.java
+++ app/src/main/java/base/BaseApplication.java
@@ -1,20 +1,10 @@
 package base;
 
 import android.app.Application;
-import android.os.Environment;
-
-import com.okhttplib.OkHttpUtil;
-import com.okhttplib.annotation.CacheType;
-import com.okhttplib.annotation.Encoding;
-import com.okhttplib.cookie.PersistentCookieJar;
-import com.okhttplib.cookie.cache.SetCookieCache;
-import com.okhttplib.cookie.persistence.SharedPrefsCookiePersistor;
-
-import java.io.File;
+import android.util.Log;
 
 /**
  * Application
- * 1、初始化全局OkHttpUtil
  * @author zhousf
  */
 public class BaseApplication extends Application {
@@ -29,31 +19,7 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         baseApplication = this;
-        String downloadFileDir = Environment.getExternalStorageDirectory().getPath()+"/okHttp_download/";
-        String cacheDir = Environment.getExternalStorageDirectory().getPath();
-        if(getExternalCacheDir() != null){
-            //缓存目录，APP卸载后会自动删除缓存数据
-            cacheDir = getExternalCacheDir().getPath();
-        }
-        OkHttpUtil.init(this)
-                .setConnectTimeout(15)//连接超时时间
-                .setWriteTimeout(15)//写超时时间
-                .setReadTimeout(15)//读超时时间
-                .setMaxCacheSize(10 * 1024 * 1024)//缓存空间大小
-                .setCacheType(CacheType.FORCE_NETWORK)//缓存类型
-                .setHttpLogTAG("HttpLog")//设置请求日志标识
-                .setIsGzip(false)//Gzip压缩，需要服务端支持
-                .setShowHttpLog(true)//显示请求日志
-                .setShowLifecycleLog(true)//显示Activity销毁日志
-                .setRetryOnConnectionFailure(false)//失败后不自动重连
-                .setCachedDir(new File(cacheDir,"okHttp_cache"))//缓存目录
-                .setDownloadFileDir(downloadFileDir)//文件下载保存目录
-                .setResponseEncoding(Encoding.UTF_8)//设置全局的服务器响应编码
-                .setRequestEncoding(Encoding.UTF_8)//设置全局的请求参数编码
-                .addResultInterceptor(HttpInterceptor.ResultInterceptor)//请求结果拦截器
-                .addExceptionInterceptor(HttpInterceptor.ExceptionInterceptor)//请求链路异常拦截器
-                .setCookieJar(new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this)))//持久化cookie
-                .build();
+        Log.d("BaseApplication","BaseApplication已初始化");
     }
 
 
---
+++ app/src/main/java/base/BaseProvider.java
@@ -0,0 +1,96 @@
+package base;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.okhttplib.OkHttpUtil;
+import com.okhttplib.annotation.CacheType;
+import com.okhttplib.annotation.Encoding;
+import com.okhttplib.cookie.PersistentCookieJar;
+import com.okhttplib.cookie.cache.SetCookieCache;
+import com.okhttplib.cookie.persistence.SharedPrefsCookiePersistor;
+
+import java.io.File;
+
+/**
+ * Author : zhousf
+ * Description : 采用占位方式初始化第三方组件（onCreate比Application的onCreate先执行）
+ * Date : 2017/7/20.
+ */
+public class BaseProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        //获取Application上下文
+        initOkHttp(this.getContext());
+        return false;
+    }
+
+    /**
+     * 初始化网络请求框架
+     */
+    void initOkHttp(Context context){
+        String downloadFileDir = Environment.getExternalStorageDirectory().getPath()+"/okHttp_download/";
+        String cacheDir = Environment.getExternalStorageDirectory().getPath();
+        if(context.getExternalCacheDir() != null){
+            //缓存目录，APP卸载后会自动删除缓存数据
+            cacheDir = context.getExternalCacheDir().getPath();
+        }
+        OkHttpUtil.init(context)
+                .setConnectTimeout(15)//连接超时时间
+                .setWriteTimeout(15)//写超时时间
+                .setReadTimeout(15)//读超时时间
+                .setMaxCacheSize(10 * 1024 * 1024)//缓存空间大小
+                .setCacheType(CacheType.FORCE_NETWORK)//缓存类型
+                .setHttpLogTAG("HttpLog")//设置请求日志标识
+                .setIsGzip(false)//Gzip压缩，需要服务端支持
+                .setShowHttpLog(true)//显示请求日志
+                .setShowLifecycleLog(true)//显示Activity销毁日志
+                .setRetryOnConnectionFailure(false)//失败后不自动重连
+                .setCachedDir(new File(cacheDir,"okHttp_cache"))//缓存目录
+                .setDownloadFileDir(downloadFileDir)//文件下载保存目录
+                .setResponseEncoding(Encoding.UTF_8)//设置全局的服务器响应编码
+                .setRequestEncoding(Encoding.UTF_8)//设置全局的请求参数编码
+                .addResultInterceptor(HttpInterceptor.ResultInterceptor)//请求结果拦截器
+                .addExceptionInterceptor(HttpInterceptor.ExceptionInterceptor)//请求链路异常拦截器
+                .setCookieJar(new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(context)))//持久化cookie
+                .build();
+        Log.d("BaseProvider","OkHttp已初始化");
+    }
+
+
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
