909a7512e5787459d4a9c2d4c11a00d604992cc8
zhousf <424427633@qq.com>
2017-08-08 17:44:05 +0800

    升级okhttp内核

--- app/src/main/java/com/okhttp3/activity/MainActivity.java
+++ app/src/main/java/com/okhttp3/activity/MainActivity.java
@@ -19,7 +19,6 @@ import com.okhttplib.annotation.RequestType;
 import com.okhttplib.callback.Callback;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 import base.BaseActivity;
 import butterknife.Bind;
@@ -168,31 +167,33 @@ public class MainActivity extends BaseActivity {
      * 异步请求：回调方法可以直接操作UI
      */
     private void async() {
-        OkHttpUtil.getDefault(this).doAsync(
-                HttpInfo.Builder()
-                        .setUrl(url)
-                        .setRequestType(RequestType.GET)//设置请求方式
-                        .addHead("head", "test")//添加头参数
-                        .addParam("param", "test")//添加接口参数
-                        .setDelayExec(2, TimeUnit.SECONDS)//延迟2秒执行
-                        .build(),
-                new Callback() {
-                    @Override
-                    public void onFailure(HttpInfo info) throws IOException {
-                        String result = info.getRetDetail();
-                        resultTV.setText("异步请求失败：" + result);
-                    }
+        for(int i=0;i<5;i++){
+            OkHttpUtil.getDefault(this).doAsync(
+                    HttpInfo.Builder()
+                            .setUrl(url)
+                            .setRequestType(RequestType.GET)//设置请求方式
+                            .addHead("head", "test")//添加头参数
+                            .addParam("param", "test")//添加接口参数
+//                            .setDelayExec(2, TimeUnit.SECONDS)//延迟2秒执行
+                            .build(),
+                    new Callback() {
+                        @Override
+                        public void onFailure(HttpInfo info) throws IOException {
+                            String result = info.getRetDetail();
+                            resultTV.setText("异步请求失败：" + result);
+                        }
 
-                    @Override
-                    public void onSuccess(HttpInfo info) throws IOException {
-                        String result = info.getRetDetail();
-                        resultTV.setText("异步请求成功：" + result);
-                        //GSon解析
-                        TimeAndDate time = new Gson().fromJson(result, TimeAndDate.class);
-                        LogUtil.d("MainActivity", time.getResult().toString());
-                        setFromCacheTV(info);
-                    }
-                });
+                        @Override
+                        public void onSuccess(HttpInfo info) throws IOException {
+                            String result = info.getRetDetail();
+                            resultTV.setText("异步请求成功：" + result);
+                            //GSon解析
+                            TimeAndDate time = new Gson().fromJson(result, TimeAndDate.class);
+                            LogUtil.d("MainActivity", time.getResult().toString());
+                            setFromCacheTV(info);
+                        }
+                    });
+        }
         needDeleteCache(true);
     }
 
